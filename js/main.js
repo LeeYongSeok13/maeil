645739a2db9b72d4b5d844801832c60d2c8bb227
LeeYongSeok13 <minamiharuka333@gmail.com>
2022-08-23 17:48:36 +0900

    20220823

--- js/main.js
+++ js/main.js
@@ -16,8 +16,34 @@ window.addEventListener('DOMContentLoaded', function () {
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
+        },
+        on: {
+            slideChange: function () {
+            }
         }
+    });
 
+
+
+    let brandBg = document.querySelector('#brand');
+    let brandLi01 = document.querySelector('#brand .container ul li:nth-child(1)');
+    let brandLi02 = document.querySelector('#brand .container ul li:nth-child(2)');
+    let brandLi03 = document.querySelector('#brand .container ul li:nth-child(3)');
+    let brandLi04 = document.querySelector('#brand .container ul li:nth-child(4)');
+    brandLi01.addEventListener('mouseover', function () {
+        brandBg.style.backgroundImage = `url('../img/bg01.jpg')`;
+    });
+
+    brandLi02.addEventListener('mouseover', function () {
+        brandBg.style.backgroundImage = `url('../img/bg02.jpg')`;
+    });
+
+    brandLi03.addEventListener('mouseover', function () {
+        brandBg.style.backgroundImage = `url('../img/bg03.jpg')`;
+    });
+
+    brandLi04.addEventListener('mouseover', function () {
+        brandBg.style.backgroundImage = `url('../img/bg04.jpg')`;
     });
 
 })
